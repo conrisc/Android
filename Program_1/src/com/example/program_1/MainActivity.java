493672ef22f60d26ad6498cdc152d71664a7fd51
Konrad Cielecki <conrisc@gmail.com>
2016-06-14 17:28:12 +0200

    Update MainActivity.java

--- Program_1/src/com/example/program_1/MainActivity.java
+++ Program_1/src/com/example/program_1/MainActivity.java
@@ -9,11 +9,11 @@ import android.widget.*;
 public class MainActivity extends Activity {
 	
 	Context context;
-    TextView title;
-    EditText text;
-    RadioGroup yn;
-    RatingBar rate;
-    Button button;
+    	TextView title;
+    	EditText text;
+    	RadioGroup yn;
+    	RatingBar rate;
+    	Button button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +22,19 @@ public class MainActivity extends Activity {
 		context = getApplicationContext();
 		
 		title = (TextView) findViewById(R.id.textView_title);
-        text = (EditText) findViewById(R.id.editText_text);
-        yn = (RadioGroup) findViewById(R.id.radioGroup_yn);
-        rate = (RatingBar) findViewById(R.id.ratingBar);
-        button = (Button) findViewById(R.id.button_confirm);
-        
-        
+		text = (EditText) findViewById(R.id.editText_text);
+        	yn = (RadioGroup) findViewById(R.id.radioGroup_yn);
+        	rate = (RatingBar) findViewById(R.id.ratingBar);
+        	button = (Button) findViewById(R.id.button_confirm);
 	}
 	
 	public void clickListener(View v) {
 		Intent intent = new Intent(context, ResultActivity.class);
-
 		RadioButton yn_a = (RadioButton)yn.findViewById(yn.getCheckedRadioButtonId());
-	    intent.putExtra("text_a", text.getText().toString());
-	    intent.putExtra("yn_a", yn_a.getText());
-	    intent.putExtra("rate_a", rate.getRating());
+	    	intent.putExtra("text_a", text.getText().toString());
+	    	intent.putExtra("yn_a", yn_a.getText());
+	    	intent.putExtra("rate_a", rate.getRating());
 		
-
-	    startActivity(intent);
+	    	startActivity(intent);
 	}
 }
