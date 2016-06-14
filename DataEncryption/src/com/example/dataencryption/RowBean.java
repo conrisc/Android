334f97f36b271ab57756d16c202ed75946982ef3
Konrad Cielecki <conrisc@gmail.com>
2016-06-14 17:25:35 +0200

    Update RowBean.java

--- DataEncryption/src/com/example/dataencryption/RowBean.java
+++ DataEncryption/src/com/example/dataencryption/RowBean.java
@@ -5,21 +5,17 @@ import android.graphics.BitmapFactory;
 
 public class RowBean {
 	 
-    public Bitmap icon;
-    public String title;
+    	public Bitmap icon;
+    	public String title;
  
-    public RowBean(){
- 
-    }
+	public RowBean(){}
  
 	public RowBean(String iconPath, String title) {
- 
-    	//String path = Environment.getExternalStorageDirectory().toString() + iconPath;
-    	Bitmap bmp = BitmapFactory.decodeFile(iconPath);
-    	
-    	Bitmap resizedBmp = Bitmap.createScaledBitmap(bmp, 150, 200, false);
+    		//String path = Environment.getExternalStorageDirectory().toString() + iconPath;
+    		Bitmap bmp = BitmapFactory.decodeFile(iconPath);
+    		Bitmap resizedBmp = Bitmap.createScaledBitmap(bmp, 150, 200, false);
     	
 		this.icon = resizedBmp;
-        this.title = title;
+        	this.title = title;
     }
 }
